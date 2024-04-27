c13952b3c0b4f1f16acd8c3fa486b664732f5844
DevAshgeek <ashutoshyad3105@gmail.com>
2024-04-27 07:10:15 +0530

    updated url routes in app.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         <Route path="*" element={<Navigate replace to={isLoggedIn ? "/todo" : "/home"} />} /> */}
         <Route
           path="/"
-          element={<Navigate replace to={isLoggedIn ? "/todo" : "/"} />}
+          element={<Navigate replace to={isLoggedIn ? "/todo" : "/home"} />}
         />
         <Route
           path="/login"
@@ -34,7 +34,7 @@ function App() {
           element={isLoggedIn ? <Todo isHomePage={isHomePage} /> : <Navigate replace to="/login" />}
         />
         <Route path="/signup" element={<Signup isHomePage={isHomePage} />} />
-        <Route path="/" element={<Home isHomePage={isHomePage} />} />
+        <Route path="/home" element={<Home isHomePage={isHomePage} />} />
         <Route
           path="*"
           element={<Navigate replace to={isLoggedIn ? "/todo" : "/"} />}
